6d392e789aa9c48c3ddaecbfd09f17348a772372
vonteacher <vaughn.weiss@gmail.com>
2024-10-10 22:47:26 -0600

    First commit

---
+++ lib/random_map.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "random_map/version"
+
+module RandomMap
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/random_map/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RandomMap
+  VERSION = "0.1.0"
+end
